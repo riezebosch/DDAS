e7f08c372faff41bd4420fb2005121fb95d969d2
Manuel Riezebosch <mriezebosch@gmail.com>
2014-10-13 10:10:54 +0200

    database first een context gemaakt

--- DatabaseFirst.Tests/UnitTest1.cs
+++ DatabaseFirst.Tests/UnitTest1.cs
@@ -9,6 +9,13 @@ namespace DatabaseFirst.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            using (var context = new SchoolEntities())
+            {
+                foreach (var person in context.People)
+                {
+                    Console.WriteLine("{0} {1}", person.FirstName, person.LastName);
+                }
+            }
         }
     }
 }
