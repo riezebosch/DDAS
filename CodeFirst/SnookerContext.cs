7f5d510cd0da91b6c0ec70d8e0a1309a4af98cdd
Manuel Riezebosch <mriezebosch@gmail.com>
2014-10-14 14:04:02 +0200

    Voorbeeldje van Fluent en Reverse Engineered van School

--- CodeFirst/SnookerContext.cs
+++ CodeFirst/SnookerContext.cs
@@ -9,5 +9,15 @@ namespace CodeFirst
     public class SnookerContext : DbContext
     {
         public IDbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Game>()
+                .HasKey(e => e.Id)
+                .Property(e => e.Location)
+                .IsRequired()
+                .HasColumnName("LoCaTiOn");
+        }
     }
 }
