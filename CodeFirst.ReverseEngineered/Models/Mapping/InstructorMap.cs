aac8b9be9b9cd09733afe93ba14ba8e8d22f1023
Manuel Riezebosch <mriezebosch@gmail.com>
2014-10-15 13:58:52 +0200

    verloren instructormap

---
+++ CodeFirst.ReverseEngineered/Models/Mapping/InstructorMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+
+namespace CodeFirst.ReverseEngineered.Models.Mapping
+{
+    public class InstructorMap : EntityTypeConfiguration<Instructor>
+    {
+        public InstructorMap()
+        {
+            base.ToTable("Instructor");
+            base.Property(t => t.HireDate).HasColumnName("HireDate");
+            base.Property(p => p.HireDate).IsRequired();
+        }
+    }
+}
