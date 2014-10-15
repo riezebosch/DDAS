0a9388fea7c7da1226f6af619b20e1ea1e9ec872
Manuel Riezebosch <mriezebosch@gmail.com>
2014-10-15 11:06:49 +0200

    Database.Log en Query.ToString

--- CodeFirst.ReverseEngineered/UnitTest1.cs
+++ CodeFirst.ReverseEngineered/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CodeFirst.ReverseEngineered.Models;
 using System.Linq;
+using System.Transactions;
 
 namespace CodeFirst.ReverseEngineered
 {
@@ -23,6 +24,40 @@ namespace CodeFirst.ReverseEngineered
             using (var context = new SchoolContext())
             {
                 Assert.IsInstanceOfType(context.People.Single(p => p.FirstName == "Peggy"), typeof(Student));
+            }
+        }
+
+        [TestMethod]
+        public void UpdatePropertyViaDeChangeTracker()
+        {
+            using (new TransactionScope())
+            using (var context = new SchoolContext())
+            {
+                context.Database.Log = Console.WriteLine;
+
+                var kim = context.People.Find(1);
+                Assert.AreEqual("Kim", kim.FirstName);
+
+                context.Entry(kim).Property(p => p.FirstName).CurrentValue = "UPDATE";
+                Assert.AreEqual("UPDATE", kim.FirstName);
+                Assert.AreEqual("Kim", context.Entry(kim).Property(p => p.FirstName).OriginalValue);
+
+                Assert.IsTrue(context.Entry(kim).Property(p => p.FirstName).IsModified);
+
+                context.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void ShowSqlFromQuery()
+        {
+            using (var context = new SchoolContext())
+            {
+                var query = from p in context.People.OfType<Instructor>()
+                            where p.FirstName == "Kim"
+                            select p;
+
+                Console.WriteLine(query);
             }
         }
     }
